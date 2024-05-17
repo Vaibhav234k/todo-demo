119987e34ae79b820d99ded302fc3426cdb384aa
vaibhav234k <sonivaibhav378@gmail.com>
2024-05-17 19:30:10 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './index.css'
+import {TodoForm} from './components/form'
+import { Lamp } from './components/lamp'
+function App() {
+
+  return (
+    <>
+      <Lamp/>
+      <TodoForm/>
+    </>
+  )
+}
+
+export default App
